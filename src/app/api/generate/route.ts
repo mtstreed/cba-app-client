de41a871ecc64ffde891a8585aa453edaa90b31d
Mateo Streed <mtstreed@gmail.com>
2025-04-15 21:34:31 -0400

    Increase maxDuration of api callout to try and fix production timeout bug.

--- src/app/api/generate/route.ts
+++ src/app/api/generate/route.ts
@@ -1,6 +1,8 @@
 import { NextResponse } from 'next/server';
 import { generateCbaDraft } from '@/app/utils/openai';
 
+export const maxDuration = 20; 
+
 export async function POST(request: Request) {
     try {
         const { projectDescription } = await request.json();
